164aceeeb8c213d63e1c9d7d84175ac21ae6ed89
abrantes09 <abrantes09@icloud.com>
2024-09-10 15:31:53 -0400

    Hello World

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "abrantes09";
+    char STUDENT_ID[] = "976020272";
 
     return 0 ;
 }
